b20a85d2d8bd548da4b2fba236e2b30fe95ac997
Rafael Lopez <50971821+raffarraffa@users.noreply.github.com>
2026-04-14 00:22:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-# from app import ingesta_txt, logging_config, parse_txt, parse_noticias
 import logging
+import argparse
 from libs.logging_config import setup_logging
-from pipelines.ingesta_txt import ingesta_txt,  parse_txt, parse_noticias
+from pipelines.ingesta_txt import ingesta_txt, parse_txt, parse_noticias
 from libs.output_file import output_file
 
 
-def main():  
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--archivo", type=str, required=True, help="Ruta del archivo a procesar")
+    args = parser.parse_args()
+
     setup_logging()
     logging.info(f"Inicio del programa")
-    contenido = ingesta_txt(file_path_name="data/noticias_crudas.txt")
+    contenido = ingesta_txt(file_path_name=args.archivo)
     noticias = parse_txt(contenido)
     noticias_parseadas = parse_noticias(noticias)
-    output_file(noticias_parseadas,"pipeline","lista")
-    logging.info(f"Fin del programa")           
+    output_file(noticias_parseadas, "pipeline", "lista")
+    logging.info(f"Fin del programa")
+
 if __name__ == "__main__":
     main()
-
